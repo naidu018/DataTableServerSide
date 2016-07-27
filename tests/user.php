4243459a98a8ad34cd507fafdfda4920a86c4128
swetha <snaidu@tecnavia.com>
2016-07-27 16:23:24 -0500

    Completely functional code for searching, sorting, paging on server side

--- tests/user.php
+++ tests/user.php
@@ -11,10 +11,45 @@ $ordervalue=$_REQUEST["order"]["0"]["column"];
 
 // Logic is the user tries to set one at each time. If he sets the ordervalue, then the following logic is accompanied. 
 //New idea is to elimintate the second condition
-if($ordervalue>0)
+if($searchvalue) 
+{
+    $sql= "select * from emails where _messageId like '%$searchvalue%' or _from like '%$searchvalue%' or _pSetup like '%$searchvalue%' or _Deadline like '%$searchvalue%' or _Subject like '%$searchvalue%' or _AssignedPerson like '%$searchvalue%'";
+   
+    $query = mysqli_query($con,$sql) or die(mysqli_error($con));
+    while($row = mysqli_fetch_array($query,MYSQLI_ASSOC)){
+    $data_row = array();
+    foreach($row as $key=>$value){
+        array_push($data_row, $value);
+    }
+    array_push($data,$data_row);
+}
+$reverse=array_reverse($data);
+//echo '<code><pre>';
+$json_to_return = array();
+$json_to_return["draw"] = $_REQUEST["draw"];
+$json_to_return["recordsTotal"] = sizeof($data);
+$json_to_return["recordsFiltered"] = sizeof($data);
+$json_to_return["data"] = $reverse;
+
+}
+
+else if($ordervalue>0|| ($ordervalue==0&&$_REQUEST["order"]["0"]["dir"]=="desc"))
 {
 	switch($ordervalue)
 	{
+		case 0:
+			$sql = "SELECT * FROM emails order by _messageId asc";
+				$query = mysqli_query($con,$sql) or die(mysqli_error($con));
+				while($row = mysqli_fetch_array($query,MYSQLI_ASSOC))
+				{
+				$data_row = array();
+				foreach($row as $key=>$value)
+				{
+					array_push($data_row, $value);
+				}
+				array_push($data,$data_row);
+				}
+				break;
 		case 1:
 			if($_REQUEST["order"]["0"]["dir"]=="asc")
 			{
